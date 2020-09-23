31764d04ab4dc5f67a46bc040f14528ef5ec79c6
elnur004 <lnur004@gmail.com>
2020-09-24 01:28:00 +0400

    Added Income and Expence constructors and structure for budget

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-    //Some code
+    var Expence = function(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value
+    };
 
+    var Income = function(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value
+    };
 
-
-
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+   
 })();
 
 
@@ -52,7 +70,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var ctrlAddItem = function() {
         // 1. Get the field input data
         var input = UICtrl.getInput();
-        
+
         // 2. Add the item to the Budget Controller
 
         // 3. Add the item to the UI
