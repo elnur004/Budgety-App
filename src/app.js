fcc130b4636f0f3165944cec2229fd24dca2178e
elnur004 <lnur004@gmail.com>
2020-09-24 00:10:11 +0400

    initialized an init function

--- src/app.js
+++ src/app.js
@@ -38,12 +38,21 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keydown', function(event) {
+            if(event.key === 'Enter') {
+                ctrlAddItem();  
+            }
+        });
+    };
 
     var ctrlAddItem = function() {
         // 1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
+        
         // 2. Add the item to the Budget Controller
 
         // 3. Add the item to the UI
@@ -51,18 +60,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget
-    }
+    };
 
-
-
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-    
-    document.addEventListener('keydown', function(event) {
-        if(event.key === 'Enter') {
-            ctrlAddItem();
-            
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    });
-
+    };
 
 })(budgetController, UIController);
+
+controller.init();
