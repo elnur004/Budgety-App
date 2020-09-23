1fd4576cf5211a3ac9aa635768a0db4e06368071
elnur004 <lnur004@gmail.com>
2020-09-23 07:26:04 +0400

    structured the DOM strings

--- src/app.js
+++ src/app.js
@@ -12,14 +12,23 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
 
     return {
         getInput: function() {
             return{
-                type: document.querySelector('.add__type').value,   // Will be either inc or exp
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,   // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
         }
     };
 
@@ -29,6 +38,7 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function() {
         // 1. Get the field input data
@@ -45,7 +55,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keydown', function(event) {
         if(event.key === 'Enter') {
