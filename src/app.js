a677688bd14047678ac29553107380bcc67c7be3
elnur004 <lnur004@gmail.com>
2020-09-26 02:23:53 +0400

    added new item to budgetController

--- src/app.js
+++ src/app.js
@@ -23,6 +23,37 @@ var budgetController = (function() {
             inc: 0
         }
     };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+
+            // Create new item based on 'inc' or 'exp'
+            if(type === 'exp') {
+                newItem = new Expence(ID, des, val);
+            } else if(type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push new item into data structure
+            data.allItems[type].push(newItem);
+
+            // Return new item
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
    
 })();
 
@@ -68,10 +99,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function() {
+        var input, newItem;
+
         // 1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the Budget Controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
