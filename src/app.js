31b881ee629d13e93e5bdcec37a2d337603acb38
elnur004 <lnur004@gmail.com>
2020-09-27 02:16:01 +0400

    converted input to a number and added a condition to prevent false value

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ var UIController = (function() {
             return{
                 type: document.querySelector(DOMstrings.inputType).value,   // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
         addListItems: function(obj, type) {
@@ -138,12 +138,23 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     };
 
+
+    var updateBudget = function() {
+        // 1. Calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+    }
+
+
     var ctrlAddItem = function() {
         var input, newItem;
 
         // 1. Get the field input data
         input = UICtrl.getInput();
 
+        if(input.description !== '' && isNaN(input.description) && !isNaN(input.value) && input.value > 0) {
         // 2. Add the item to the Budget Controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
@@ -153,9 +164,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Clear the fields
         UICtrl.clearFields();
 
-        // 5. Calculate the budget
-
-        // 5. Display the budget
+        // 5. Calculate and update budget
+        updateBudget();
+        }
     };
 
     return {
