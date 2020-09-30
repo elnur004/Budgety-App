b8010277c1b68aeee29c6b05df914dc98c5a69e6
elnur004 <lnur004@gmail.com>
2020-10-01 02:13:10 +0400

    created methods to calculate the budget and the percentage

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ var budgetController = (function() {
         this.value = value
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        })
+        data.totals[type] = sum;
+    }
+
     var data = {
         allItems: {
             exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -35,7 +45,6 @@ var budgetController = (function() {
                 ID = 0;
             }
             
-
             // Create new item based on 'inc' or 'exp'
             if(type === 'exp') {
                 newItem = new Expence(ID, des, val);
@@ -48,6 +57,32 @@ var budgetController = (function() {
 
             // Return new item
             return newItem;
+        },
+
+        calculateBudget: function() {
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+
+        },
+
+        returnBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
         },
 
         testing: function() {
@@ -141,10 +176,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var updateBudget = function() {
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.returnBudget();
 
         // 3. Display the budget on the UI
+        console.log(budget);
     }
 
 
